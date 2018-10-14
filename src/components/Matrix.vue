02bd75fa067ccfe0dd51ca926b4981659ac5e435
LightDiscord <root@arnaud.sh>
2018-10-14 11:48:57 +0200

    Select result mode

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -4,10 +4,8 @@
 
     <table class="table">
       <tr v-for="(line, y) in matrix" :key="y">
-        <td v-for="(item, x) in line" :key="x"
-          :class="{ selected: item }"
-          @click="input(x, y)">
-          {{ item ? 1 : 0 }}
+        <td v-for="(bit, x) in line" :key="x" :class="{ selected: bit }" @click="toggle({ x, y })">
+          {{ bit ? 1 : 0 }}
         </td>
       </tr>
     </table>
@@ -16,26 +14,35 @@
       <i>Click on a box to toggle it!</i>
     </p>
 
-    <br>
-
     <h2 class="title is-4">Result</h2>
 
     <pre><code class="cpp"><span v-html="highlight"></span></code></pre>
+
+    <div class="field">
+      <label class="label">Result mode</label>
+      <div class="control">
+        <div class="select">
+          <select v-model="mode">
+            <option v-for="mode in $options.modes" :key="mode.name" :value="mode">
+              {{ mode.name | capitalize }}
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-
 import hljs from 'highlight.js';
 import 'highlight.js/lib/languages/arduino';
 
-import * as Utils from '../utils';
+import { parseLines, modes } from '../utils';
 
-const format = (list) => {
-  const content = list.map(Utils.decimal).join(', ');
+const format = (mode, list) => {
+  const content = list.map(mode).join(', ');
 
-  return `byte name[8] = { ${content} };`;
+  return `byte name[${list.length}] = { ${content} };`;
 };
 
 export default {
@@ -43,24 +50,34 @@ export default {
     matrix: Array,
     index: Number,
   },
+  data() {
+    return {
+      mode: modes[Math.floor(Math.random() * modes.length)],
+    };
+  },
   methods: {
-    ...mapActions(['toggle']),
-
-    input(x, y) {
-      this.toggle({
+    toggle(coords) {
+      this.$store.dispatch('toggle', {
         index: this.index,
-        coords: { x, y },
+        coords,
       });
     },
   },
   computed: {
     result() {
-      return Utils.parseLines(this.matrix);
+      return parseLines(this.matrix);
     },
     highlight() {
-      return hljs.highlight('arduino', format(this.result)).value;
+      return hljs.highlight('arduino', format(this.mode, this.result)).value;
     },
   },
+  filters: {
+    capitalize(value) {
+      if (!value) return '';
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+  },
+  modes,
 };
 </script>
 
@@ -72,5 +89,13 @@ export default {
 
 td {
   cursor: pointer;
+}
+
+h2.title {
+  margin-top: 1.5rem;
+}
+
+div.field {
+  margin-top: 1rem;
 }
 </style>
